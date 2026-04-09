0a5e6cd86e253e675045e33e70304d2578f55431
cyber-goblinbug <wjw67@miami.edu>
2026-04-09 14:01:44 +0000

    trdf

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('hbs');
+const fs = require('fs');
+const  Sequelize  = require('sequelize');
+const  DataTypes  = require('sequelize');
+
+
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -22,10 +28,45 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
+//Register Partials
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+hbs.registerPartial('partial_name','partial value');
+
+
+
+
+// Setup out Database
+const dataDirectory = path.join(__dirname, 'data');
+const storage = path.join(dataDirectory, 'database.sqlite');
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  dialectModule: require('sqlite3'),
+  storage,
+  logging: true
+});
+const Task = sequelize.define('Task', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false}, 
+    description: {type: DataTypes.TEXT,allowNull: false}
+});
+async function syncDB() {
+  await sequelize.sync( );
+
+};
+syncDB().catch(console.error);
+
+//Get Home Page
+app.get('/', function(req, res)  {
+  res.render('index', { title: 'Miami' });
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
